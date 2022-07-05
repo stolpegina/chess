24e7434663400cd08788fcc17fac68714a6b8452
Svetlana Tolpegina <stolpegina@gmail.com>
2022-07-05 15:56:55 +0400

    Added lost figures

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import BoardComponent from "./components/BoardComponent";
+import LostFigures from "./components/LostFigures";
 import { Board } from "./models/Board";
+import { Colors } from "./models/Colors";
+import { Player } from "./models/Player";
 
 function App() {
   const [board, setBoard] = useState(new Board());
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
     restart();
+    setCurrentPlayer(whitePlayer);
   }, []);
 
   function restart() {
@@ -17,9 +24,24 @@ function App() {
     setBoard(newBoard);
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(
+      currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer
+    );
+  }
+
   return (
     <div className="app">
-      <BoardComponent board={board} setBoard={setBoard} />
+      <BoardComponent
+        board={board}
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
+      <div>
+        <LostFigures title="Черные фигуры" figures={board.lostBlackFigures} />
+        <LostFigures title="Белые фигуры" figures={board.lostWhiteFigures} />
+      </div>
     </div>
   );
 }
